90011215c341ec1abdde86283e9d402ac168f135
christ <larrabyghost@gmail.com>
2021-12-26 03:19:39 +0100

    premiere commit des 2 premieres pages creer

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from 'react-dom'
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'
+import reportWebVitals from "./reportWebVitals";
+import Home from "./pages/Home";
+import Onboarding from "./pages/Onboarding/Onboarding";
+import ProfilClasse from "./pages/ProfilClasse/ProfilClasse";
+import Error from "./pages/Error";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+     <Router>
+        <Routes>
+          <Route exact path="/" element={<Onboarding/>}/>
+          <Route exact path="/profil" element={<ProfilClasse/>}/>
+          <Route exact path="/home" element={<Home/>}/>
+          <Route path="*" element={<Error/>}/>
+        </Routes>
+    </Router>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
